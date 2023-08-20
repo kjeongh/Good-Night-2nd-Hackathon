acc0e92957654d7122a12213ccc23506a36882d3
jhkim <wellsy001012@gmail.com>
2023-08-21 06:53:31 +0900

    chore: 프로젝트 생성

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './modules/movie/entities/movie.entity';
+import { MoviesModule } from './modules/movie/movies.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'hackathon',
+      password: '1012',
+      database: 'hackathondb',
+      entities: [Movie],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
